ec50e26c670716a6a79347cc60cbd1570c2fbcb7
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2020-07-29 15:16:50 -0700

    fixing package.json

---
+++ build/precache-manifest.a2f3d489fb9078c4ac9302ccf6702efd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d6e3cf03e084b65cefaa5c0c0db252",
+    "url": "/rebeccachavezlv/robofriends.git/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/rebeccachavezlv/robofriends.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a0d4817907c6d554206f",
+    "url": "/rebeccachavezlv/robofriends.git/static/css/main.584a2422.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/rebeccachavezlv/robofriends.git/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rebeccachavezlv/robofriends.git/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d4817907c6d554206f",
+    "url": "/rebeccachavezlv/robofriends.git/static/js/main.d071cbae.chunk.js"
+  },
+  {
+    "revision": "3db5e616c69d26bf4f54",
+    "url": "/rebeccachavezlv/robofriends.git/static/js/runtime-main.12322b62.js"
+  }
+]);
